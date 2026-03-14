b4db538ea1e02033e0f79caf8144282e8d86b237
Abu Abdullah Umar <itshabib.me+PH@gmail.com>
2026-03-14 06:12:40 +0600

    refactor: Removed <ol> tag

--- src/Post.jsx
+++ src/Post.jsx
@@ -2,11 +2,9 @@ export default function Post({ post }) {
   return (
     <div>
       {/* Displaying the fetch data title */}
-      <ol>
-        <li>
-          <h5>{post.title}</h5>
-        </li>
-      </ol>
+      <li>
+        <h5>{post.title}</h5>
+      </li>
     </div>
   );
 }
